75a4a623ec35cd190bb378ca2caf68937e47cec2
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-01-09 16:49:24 +0700

    first commit

---
+++ database/migrations/2023_01_09_070506_create_detail_program.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_program', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('program_kegiatan_id');
+            $table->foreign('program_kegiatan_id')->references('id')->on('program_kegiatan');
+            $table->date('tanggal');
+            $table->string('nama_kegiatan');
+            $table->string('pengeluaran');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_program');
+    }
+};
