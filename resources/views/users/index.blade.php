3258c88a700d79cfc44748aa303e04c200123c86
unknown <danirizky704@gmail.com>
2023-01-14 12:23:37 +0700

    copas bootstrap

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,37 +6,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 
 <body>
-    <a href="{{ route('users.create') }}">Tambah</a>
-    <table>
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Role</th>
-                <th>Nama</th>
-                <th>Username</th>
-                <th>Password</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($users as $user => $value)
-                <tr>
-                    <th>{{ $user + 1 }}</th>
-                    <th>{{ $value->role }}</th>
-                    <th>{{ $value->name }}</th>
-                    <th>{{ $value->username }}</th>
-                    <th>{{ $value->password }}</th>
-                    <th>
-                        <a href="{{ route('users') }}/{{ $value->id }}/edit">Edit</a> |
-                        <a href="javascript:void(0)" data-id="{{ $value->id }}" class="btn-delete">Hapus</a>
-                    </th>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <h1 class="text-center mb-4">Data User</h1>
+
+    <div class="container">
+        <a href="{{ route('users.create') }}" class="btn btn-primary">Tambah</a>
+
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Role</th>
+                        <th>Nama</th>
+                        <th>Username</th>
+                        <th>Password</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user => $value)
+                        <tr>
+                            <th>{{ $user + 1 }}</th>
+                            <th>{{ $value->role }}</th>
+                            <th>{{ $value->name }}</th>
+                            <th>{{ $value->username }}</th>
+                            <th>{{ $value->password }}</th>
+                            <th>
+                                <a href="{{ route('users') }}/{{ $value->id }}/edit">Edit</a> |
+                                <a href="javascript:void(0)" data-id="{{ $value->id }}" class="btn-delete">Hapus</a>
+                            </th>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"
         integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
