e424b8f210bdc699a3a5655bb487eb0044610f23
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-01-15 09:55:47 +0700

    protected routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ProgramKegiatanController;
@@ -17,21 +17,25 @@ use App\Http\Controllers\ProgramKegiatanController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-// Users route
-Route::get('/users', [UserController::class, 'index'])->name('users');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
-//Program Kegiatan route
-Route::get('/prokeg', [ProgramKegiatanController::class, 'index'])->name('prokeg');
-Route::get('/prokeg/create', [ProgramKegiatanController::class, 'create',])->name('prokeg.create');
-Route::post('/prokeg/store', [ProgramKegiatanController::class, 'store'])->name('prokeg.store');
 // Login route
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'create']);
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
+
+// Protected route
+Route::group(['middleware' => ['auth']], function () {
+    // Users route
+    Route::get('/users', [UserController::class, 'index'])->name('users');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
+    //Program Kegiatan route
+    Route::get('/prokeg', [ProgramKegiatanController::class, 'index'])->name('prokeg');
+    Route::get('/prokeg/create', [ProgramKegiatanController::class, 'create',])->name('prokeg.create');
+    Route::post('/prokeg/store', [ProgramKegiatanController::class, 'store'])->name('prokeg.store');
+});
