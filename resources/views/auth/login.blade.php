7926fac4c52d5df437876f17315bdf99983d1216
Dirham Triyadi <dirhamtriyadi@gmail.com>
2023-01-16 21:37:33 +0700

    dani asw

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,81 +2,80 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Log in</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Log in</title>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('adminlte') }}/plugins/fontawesome-free/css/all.min.css">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="{{ asset('adminlte') }}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('adminlte') }}/dist/css/adminlte.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('adminlte') }}/plugins/fontawesome-free/css/all.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="{{ asset('adminlte') }}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('adminlte') }}/dist/css/adminlte.min.css">
 </head>
 
 @if (session()->has('success'))
-        <div class="alert alert-success" role="alert">
-            {{ session('success') }}
-        </div>
-    @endif
-    @if (session()->has('loginError'))
-        <div class="alert alert-danger" role="alert">
-            {{ session('loginError') }}
-        </div>
-    @endif
+    <div class="alert alert-success" role="alert">
+        {{ session('success') }}
+    </div>
+@endif
+@if (session()->has('loginError'))
+    <div class="alert alert-danger" role="alert">
+        {{ session('loginError') }}
+    </div>
+@endif
 
 <body class="hold-transition login-page">
     <div class="login-box">
-      <div class="login-logo">
-        <a href="../../index2.html"><b>Kelompok</b> 3</a>
-      </div>
-      <!-- /.login-logo -->
-      <div class="card">
-        <div class="card-body login-card-body">
-          <p class="login-box-msg">Login </p>
+        <div class="login-logo">
+            <a href="../../index2.html"><b>Kelompok</b> 3</a>
+        </div>
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Login </p>
 
-            <form action="/login" method="POST">
+                <form action="/login" method="POST">
                     @csrf
                     <div class="input-group mb-3">
-                    <input type="username" name="username" id="name" placeholder="username">
+                        <input type="text" class="form-control" name="username" id="username" placeholder="username">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-user"></span>
                             </div>
-                          </div>
                         </div>
-                        <div class="input-group mb-3">
-                    <input type="password" name="password" id="name" placeholder="password">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" name="password" id="name" placeholder="password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
                         </div>
-                      </div>
+                    </div>
 
-                    <div class="col-4">
-                        <input type="submit" name="submit" class="btn btn-primary btn-block" value="Login"></input>
-                      </div>
-            </form>
+                    <input type="submit" name="submit" class="btn btn-primary btn-block" value="Login"></input>
+                </form>
 
-            <p class="mb-0">
-                <a href="/register" class="text-center">Register</a>
-              </p>
+                <p class="mb-0">
+                    <a href="/register" class="text-center">Register</a>
+                </p>
             </div>
-        <!-- /.login-card-body -->
+            <!-- /.login-card-body -->
+        </div>
     </div>
-  </div>
-<!-- /.login-box -->
+    <!-- /.login-box -->
 
-<!-- jQuery -->
-<script src="{{ asset('adminlte') }}/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="{{ asset('adminlte') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="{{ asset('adminlte') }}/dist/js/adminlte.min.js"></script>
+    <!-- jQuery -->
+    <script src="{{ asset('adminlte') }}/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('adminlte') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('adminlte') }}/dist/js/adminlte.min.js"></script>
 
 </body>
 
