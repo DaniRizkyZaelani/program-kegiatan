3d88288abd2fad1336a671df590331daf4862b06
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-01-24 14:59:15 +0700

    add style width 100px

--- resources/views/prokeg/index.blade.php
+++ resources/views/prokeg/index.blade.php
@@ -144,7 +144,7 @@
                                     <th>Tanggal</th>
                                     <th>Pengeluaran</th>
                                     <th>Bukti</th>
-                                    <th>Aksi</th>
+                                    <th style="width: 100px">Aksi</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -214,8 +214,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"
         integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js" integrity="sha512-CryKbMe7sjSCDPl18jtJI5DR5jtkUWxPXWaLCst6QjH8wxDexfRJic2WRmRXmstr2Y8SxDDWuBO6CQC6IE4KTA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     </script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
@@ -337,7 +337,7 @@
                                 '</td>';
 
                             html += '<td>' + item.nama_kegiatan + '</td>';
-                            html += '<td>' + item.tanggal + '</td>';
+                            html += '<td>' + moment().format() + '</td>';
                             html += '<td>' + item.pengeluaran + '</td>';
                             if (item.bukti.split('.').pop() == 'pdf') {
                                 html += '<td>' + item.bukti + '</td>';
